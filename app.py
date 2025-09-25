2eb29f91eed39b547c64a805ec7b2138efaa9fd6
Pedro brunet <joaopedrobrunet@hotmail.com>
2025-09-24 23:50:51 -0300

    class product

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite///ecommerce.db'
 
-# Definindo a rota raiz (pagina inicial) e a função que sera realizada ao executar 
+db = SQLAlchemy(app)
+
+class product(db.Model):
+    # model = caminho bd = (db).Tipo
+    # nullable = CAMPO OBRIGATORIO
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
 
 # / dentro da rota significa pagina inicial 
 @app.route('/')
-
 def hello_word() :
     return 'hello world'
 
