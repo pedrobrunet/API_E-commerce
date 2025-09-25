b216954d1b4af798ea49bbe0b91eaa7b48695d96
Pedro brunet <joaopedrobrunet@hotmail.com>
2025-09-25 16:11:08 -0300

    rota de delete product

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,11 +17,25 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
-    product = Product(name= data["name"],price= data["price"],description = data["description"])
-    db.session.add(product)
-    db.session.commit()
-    return "Cadastrado com sucesso"
+    if 'name' in data and 'price' in data:
+        product = Product(name= data["name"],price= data["price"],description = data["description"])
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message":"product accept"})
+    return jsonify({"message":"invalid product data"}), 400
 
+@app.route('/api/products/delete/<int:product_id>,' methods=["DELETE"])
+def delete_product(product_id):
+    #Recuperar o produto de dados
+    #Verificar se o produto existe
+    #Se existe, apagar do banco de dados
+    #Se não existe, retornar 404 (not found)
+    product= Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message":"product deleted sucessfully"})
+    return jsonify({"message":"product not found"}), 404
 # / dentro da rota significa pagina inicial 
 @app.route('/')
 def hello_word() :
