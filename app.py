90c0c22a92670c290a81d98ccf7e4b9967fc61c9
Pedro brunet <joaopedrobrunet@hotmail.com>
2025-09-27 18:32:17 -0300

    adiciona funcionalidade de login na API

--- app.py
+++ app.py
@@ -23,6 +23,18 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+   
+    user = User.query.filter_by(username=data.get('username')).first()
+    
+    if user and data.get('password') == user.password:
+        return jsonify({"message":"login successful"})
+    return jsonify({"message":"invalid credentials"}), 401    
+
+
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
